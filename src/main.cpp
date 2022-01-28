c49436c92bbd9f26be14b60c555423c08f45d042
hwangswan <mrthedzam@gmail.com>
2022-01-29 00:42:45 +0700

    New cat!

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,9 @@ int main() {
 
   std::cout << meovantomy.sayHello() << '\n';
 
+  Meow meovancop("Felis Domesticus Tigerus", "tabby");
+  
+  std::cout << meovancop.sayHello() << '\n';
+
   return 0;
 }
